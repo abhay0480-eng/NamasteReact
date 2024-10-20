c071291493073343108f30a0bf21566e3080a80e
Abhay Kumar <abhaykumar@Abhays-MacBook-Air.local>
2024-10-20 15:40:55 +0530

    change in app

--- Namaste_React_01/Basic04/App.js
+++ Namaste_React_01/Basic04/App.js
@@ -1,3 +1,3 @@
-const headEl = React.createElement("h1", { className: "text-lg font-bold p-10" }, "Welcome to Reactjs Library" );
+const headingEl = React.createElement("h1", { className: "text-lg font-bold p-10" }, "Welcome to Reactjs Library" );
 const rootEl = ReactDOM.createRoot(document.getElementById("root"))
-rootEl.render(headEl)
+rootEl.render(headingEl)
