d83e7e14eb92386437f5f0d81915fb95e891f4ee
Abhay Kumar <abhaykumar@Abhays-MacBook-Air.local>
2024-10-22 17:18:34 +0530

    finish episode 3 build foundation

--- Namaste_React_02/App.js
+++ Namaste_React_02/App.js
@@ -2,5 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const headingEl = React.createElement("h1", { className: "text-lg font-bold p-10" }, "Welcome to Reactjs Library" );
+
+const HeadingComponent1 = () => {
+    return <h1>Welcome to Babel</h1>
+}
+
+const HeadingComponent = () => {
+    return (
+        <div>
+        <h1>Welcome to react js library</h1>
+        <HeadingComponent1/>
+        </div>
+)
+}
 const rootEl = ReactDOM.createRoot(document.getElementById("root"))
-rootEl.render(headingEl)
+rootEl.render(<HeadingComponent/>)
